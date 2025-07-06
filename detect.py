7b74aebcc8640345f4e151c8c00c217896c3f887
UltralyticsAssistant <web@ultralytics.com>
2025-07-06 12:18:13 +0000

    Auto-format by https://ultralytics.com/actions

--- detect.py
+++ detect.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
         source="datasets/VisDrone/test/images",
         save=True,
         data="datasets/UAV_yaml/VisDrone.yaml",  # yaml 文件路径
-        batch=4
+        batch=4,
     )
